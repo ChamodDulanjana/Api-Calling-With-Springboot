ec7e58d021beea9b307edaac7968b171b5a98f82
ChamodDulanjana <chamodperera128@gmail.com>
2024-08-28 12:49:41 +0530

    Configuring Project

---
+++ src/main/java/com/chamoddulanjana/testapi/TestApiWithSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.chamoddulanjana.testapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApiWithSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApiWithSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chamoddulanjana/testapi/TestApiWithSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chamoddulanjana.testapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApiWithSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
