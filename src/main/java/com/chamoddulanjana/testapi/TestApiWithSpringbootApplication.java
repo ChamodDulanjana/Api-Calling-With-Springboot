56269356183dafd53649aed87d9aed7b2f82531b
ChamodDulanjana <chamodperera128@gmail.com>
2024-08-29 15:17:11 +0530

    Making Http request using Rest templates

--- src/main/java/com/chamoddulanjana/testapi/TestApiWithSpringbootApplication.java
+++ src/main/java/com/chamoddulanjana/testapi/TestApiWithSpringbootApplication.java
@@ -2,12 +2,19 @@ package com.chamoddulanjana.testapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class TestApiWithSpringbootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestApiWithSpringbootApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
